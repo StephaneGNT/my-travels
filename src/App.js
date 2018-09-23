5b5c521dd8da8006ad848eeef1b01e872a10aff3
Stéphane Guinot <sguinot86@gmail.com>
2018-09-23 12:54:30 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+const Travel = props => (
+  <div>  
+    <p>{props.destination}</p>
+    <p>{props.country}</p>
+    <img src={props.image} alt={props.character} />
+    <p>{props.distance}</p>
+    <hr></hr>
+  </div>
+);
+
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+          <Travel 
+          destination="Perito Moreno"
+          country="Argentine"
+          image="https://images.salaun-holidays.com/(Image)-image-Argentine-Perito-Moreno-glagier-vue-panoramique-531-fo_91066032-09032017.jpg"
+          character="Photo du Perito Moreno"
+          distance="12 989 km (environ)"
+        />
+
+        <Travel 
+          destination="Bryce Canyon"
+          country="USA"
+          image="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/USA_10654_Bryce_Canyon_Luca_Galuzzi_2007.jpg/800px-USA_10654_Bryce_Canyon_Luca_Galuzzi_2007.jpg"
+          character="Photo de Bryce Canyon"
+          distance="8 819 km (environ aussi)"
+        />
+      </div>
+      );
+  }
+}
+
+export default App;
